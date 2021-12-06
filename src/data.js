5a7208e4910d4fbbecabfe8c6b44020d66d177b8
Roopak <priydarshiroopak@gmail.com>
2021-12-07 02:10:47 +0530

    Added dummy products

--- src/data.js
+++ src/data.js
@@ -168,4 +168,30 @@ export const storeProducts = [
     count: 0,
     total: 0
   },
+  {
+    id: "LP13",
+    category: "laptop",
+    title: "Samsung M32",
+    img: "img/samsung_M32.png",
+    price: 16,
+    company: "SAMSUNG",
+    info:
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: "TB13",
+    category: "tablet",
+    title: "Samsung M32",
+    img: "img/samsung_M32.png",
+    price: 16,
+    company: "SAMSUNG",
+    info:
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
 ];
